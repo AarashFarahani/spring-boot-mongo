0d594b060b68aab0f0418be9b17cea9eefbdeee9
Arash <arashfarahani@hotmail.com>
2019-06-20 08:35:05 +0430

    mongodb template has been used

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -1,17 +1,33 @@
 package com.example.controller;
 
-import com.example.model.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/customer")
 public class CustomerController {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     private static final String template = "Hello, %s!";
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(1,
-                String.format(template, name));
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        MongoCollection<Document> collection = this.mongoTemplate.getDb().getCollection("customer");
+        DBCursor cursorDoc = collection.find();
+        while (cursorDoc.hasNext()) {
+            System.out.println(cursorDoc.next());
+        }
+    }
+
+    @PostMapping(value = "/addCustomer", headers="Accept=application/json")
+    public void addCustomer(@RequestBody String customer) {
+        Document doc = Document.parse(customer);
+        this.mongoTemplate.insert(doc, "customer");
     }
 }
